6843349fe486094c91122a19e93bdba9ca415b22
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-05-16 21:21:04 +0100

    Created: class DataService

--- src/app/pages/store/products-page/products-page.component.ts
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../service/data.service';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
 })
 export class ProductsPageComponent {
+  constructor(data: DataService) {}
 
+  ngOnInit() {}
 }
