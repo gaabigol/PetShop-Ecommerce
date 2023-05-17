9124b7e2ad055b4ba8ccc5ea594bb62124439ccc
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-05-17 20:18:01 +0100

    product-card component created

--- src/app/pages/store/products-page/products-page.component.ts
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../service/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
 })
-export class ProductsPageComponent {
-  constructor(data: DataService) {}
+export class ProductsPageComponent implements OnInit {
+  public products$: Observable<any[]> | undefined;
 
-  ngOnInit() {}
+  constructor(private data: DataService) {}
+
+  ngOnInit() {
+    this.products$ = this.data.getProducts();
+  }
 }
