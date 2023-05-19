7bf583290fb088dda06396d3029783eb7274dc98
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-05-19 22:49:54 +0100

    logout method and event created

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { Security } from 'src/app/util/security.util';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {
+export class NavbarComponent  implements OnInit {
+
+  public user: User| null;
+  public loggingOut = false;
+
+  constructor(private router: Router){
+
+  }
+  ngOnInit(): void {
+    this.user = Security.getUser();
+  }
+
+  logout(): void {
+    this.loggingOut = true;
+    setTimeout(() => {
+      Security.clear();
+      this.router.navigateByUrl('/login');
+    }, 2000);
+  }
 
 }
